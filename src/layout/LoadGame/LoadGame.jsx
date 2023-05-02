eb1b1287e205fab686a4f2625c769dbe3fcd5dcf
Sierpe <feroa515@hotmail.com>
2023-05-02 16:17:36 +0200

    fix: loadGame data format

--- src/layout/LoadGame/LoadGame.jsx
+++ src/layout/LoadGame/LoadGame.jsx
@@ -12,6 +12,7 @@ import { addBadge } from '../badgeSlice'
 import { TurnPhone } from '../../components/TurnPhone/TurnPhone'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import dayjs from 'dayjs'
 // import lgImg1 from '../../image/lgImg1.png'
 // import lgImg2 from '../../image/lgImg2.png'
 
@@ -161,7 +162,7 @@ export const LoadGame = () => {
                                         onClick={() => selectedSavedGame(load)} 
                                         >
                                             <div>Game:<strong> {load.select_games.name} </strong></div> 
-                                            <div>Saved at:<strong> {load.updated_at} </strong></div> 
+                                            <div>Saved at:<strong> {dayjs(load.updated_at).format("DD MMMM YYYY HH:mm")} </strong></div> 
                                         </div>
                                         <div className='deleteIcon' onClick={() => {setSelectedLoadGame(load), setModalShow(true)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
