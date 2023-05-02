6ad5f9541fbb97ec2c6d7dce5336a0dc818293d0
Sierpe <feroa515@hotmail.com>
2023-05-02 19:52:13 +0200

    feat: stage 0205 solution at stage 0301

--- src/components/ClueBox/ClueBox.jsx
+++ src/components/ClueBox/ClueBox.jsx
@@ -15,6 +15,9 @@ export const ClueBox = () => {
     const dispatch = useDispatch();
     console.log(clueRdx);
     console.log(badgeRdx.selectedBadge);
+    console.log(gameRdx);
+
+    const array = gameRdx.choosenGame.games_stages;
 
     const closeClue = () => {
         // let guideDialogue = document.getElementById('clueBox');
@@ -41,7 +44,9 @@ export const ClueBox = () => {
         <div className='clue'>
             {clueRdx.clueState == true ? (
                 <div className='clueBox'>
-                    <div></div>
+                    <div className='clueText text-center'>
+                        {gameRdx.choosenGame.games_stages[array.length -1].stage_id == 3 ? ("hola") : ("")}
+                    </div>
                     <div className='closeDialogue' onClick={()=> {closeClue()}}>Close 
                         <svg className='closeIcon' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
--- src/layout/Stages/Stage03/Stage0301.jsx
+++ src/layout/Stages/Stage03/Stage0301.jsx
@@ -293,6 +293,16 @@ export const Stage0301 = () => {
     }            
   };
 
+  const showSolution = () => {
+    let solution = document.getElementById('solutionBox');
+    solution.classList.add('showSolBox')
+  }
+
+  const closeSolution = () => {
+    let solution = document.getElementById('solutionBox');
+    solution.classList.remove('showSolBox')
+  }
+
   function MyVerticallyCenteredModal(props) {
     return (
       <Modal
@@ -325,6 +335,9 @@ export const Stage0301 = () => {
 
   const [modalShow, setModalShow] = React.useState(false);
 
+  // const array = gameRdx.choosenGame.games_stages;
+  // En esta no hace falta, pero en las siguientes sí.
+
   return (
     <Container
       fluid
@@ -333,6 +346,25 @@ export const Stage0301 = () => {
       <TurnPhone/>
       <Row>
         <ClueBox/>
+        <div className="solution" onClick={() => {showSolution()}}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/>
+          </svg>
+        </div>
+        <div className="solutionBox" id="solutionBox">
+          <div className="solutionText text-center">
+            <p>¡Lo has hecho bien! Pero vamos a revisar la respuesta.</p>
+            <p>Lo primero que hay que observar es que Gaara y Akumato son de tipos opuestos, puesto que Akumato contradice a Gaara. Así pues, de entre los dos, uno es legal y el otro, caótico. Pero si Kyuroo fuera legal, entonces habría dos legales, y por tanto Kyuroo no hubiese mentido diciendo que había solamente uno. 
+              Por otra parte, si Kuyroo fuera caótico, entonces sería cierto que sólo había un legal; pero entonces Kuyroo, siendo caótico, no podría haber dicho una verdad. 
+              Por lo tanto, Kuyroo no pudo haber dicho que sólo había un caballero. Así Gaara mintió al decir lo que había dicho Kyuroo, y de este modo Gaara es caótico y Akumato legal.
+            </p>
+          </div>
+          <div className='closeDialogue' onClick={()=> {closeSolution()}}>Close 
+            <svg className='closeIcon' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            </svg>
+          </div>
+        </div>
         <div className="box03">
           <div className="img1Box03">
             <OverlayTrigger
